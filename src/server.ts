42444f5551ef2b90ed63d3fc04a24472e276630e
cuttleman <silluat11@gmail.com>
2022-12-23 10:23:02 +0900

    ping pong

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,6 @@ import bodyParser from "body-parser";
 dotenv.config();
 
 const token = process.env.SLACK_TOKEN;
-const channelId = process.env.CHANNEL_ID;
 
 const web = new WebClient(token);
 const app = express();
@@ -18,14 +17,28 @@ app.get("/", (req, res) => {
 });
 app.post("/", (req, res) => {
   const slack_event = req.body;
-  if ("challenge" in slack_event) {
-    res
-      .setHeader("content-type", "application/json")
-      .status(200)
-      .json(slack_event["challenge"]);
-  }
+
   console.log(slack_event);
-  res.json({ response: "hello world!" });
+
+  if ("challenge" in slack_event) {
+    res.status(200).json(slack_event["challenge"]);
+  }
+
+  if ("event" in slack_event) {
+    const channelId = slack_event["event"]["channel"];
+    if (slack_event["event"]["text"].includes("라라")) {
+      web.chat.postMessage({
+        channel: channelId,
+        text: "from 슬랙봇",
+      });
+
+      res.status(200).json({ status: "success" });
+    }
+
+    res.status(400).json({ status: "fail" });
+  }
+
+  res.status(400).end();
 });
 
 app.listen(4000, () => {
